fa9baf6eb16733a75fa9eb55e79dc2a7a80d579d
Chris Archer <archershot@hotmail.com>
2019-07-23 17:10:27 -0500

    delete button functional. added more styling. added html / css for seachbar

--- src/todo-list/todo.js
+++ src/todo-list/todo.js
@@ -4,6 +4,9 @@ var itemList = document.querySelector('.u_list');
 // Form Submit Event
 button.addEventListener('click', addItem);
 
+// Delete Event - Delete Item
+itemList.addEventListener('click', removeItem);
+
 // Add Item
 function addItem(e){
   e.preventDefault();
@@ -11,14 +14,36 @@ function addItem(e){
   //Get input value
   var newItem = document.querySelector('.form_text').value;
 
-
   //Create New li Element
   var li = document.createElement('li');
   //Add Classname (if needed)
 
   //Add Text Node with input value
   li.appendChild(document.createTextNode(newItem));
-  itemList.appendChild(li);
-  
 
+  //Create Delete Button 
+  var deleteBtn = document.createElement('button');
+
+  //Add Delete Btn Classses
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+  //Append Text Node
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  //Append Button to li
+  li.appendChild(deleteBtn);
+
+  //Append li to list
+  itemList.appendChild(li);
+  console.log();
+}
+
+//Remove Item
+function removeItem(e) {
+  if (e.target.classList.contains('delete')) {
+    if(confirm('Are you absolutely certain?')) {
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
 }
