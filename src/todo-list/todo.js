d7f0b6ae84d0dd648606f7160c3ff66dc992304d
Chris Archer <archershot@hotmail.com>
2019-07-23 13:50:25 -0500

    added eventListener to submit button. added create element li to successfully create new li on submit click. added bootstrap delete button: currently non-functional

--- src/todo-list/todo.js
+++ src/todo-list/todo.js
@@ -1,4 +1,24 @@
-let submit = document.querySelector('.form_button');
-let enteredItem = document.querySelector('.form_text');
-let unorderedList = document.querySelector('.u_list');
+var button = document.querySelector('.form_button');
+var itemList = document.querySelector('.u_list');
 
+// Form Submit Event
+button.addEventListener('click', addItem);
+
+// Add Item
+function addItem(e){
+  e.preventDefault();
+
+  //Get input value
+  var newItem = document.querySelector('.form_text').value;
+
+
+  //Create New li Element
+  var li = document.createElement('li');
+  //Add Classname (if needed)
+
+  //Add Text Node with input value
+  li.appendChild(document.createTextNode(newItem));
+  itemList.appendChild(li);
+  
+
+}
