250b84d04c9101170549c68e2a4aed3d9a57d841
Chris Archer <archershot@hotmail.com>
2019-07-25 20:03:44 -0500

    through p130 duckett

--- src/code-along/duckett/jsbook_files/c03/js/document-object.js
+++ src/code-along/duckett/jsbook_files/c03/js/document-object.js
@@ -1,12 +1,6 @@
-// Create a variable called msg to hold a message that will be shown on the page
-// Find the title of the document, and put this in the msg variable
-var msg = '<p><b>page title: </b>' + document.title + '<br />';
-// Find the URL of the document and add it to the msg variable
-msg += '<b>page address: </b>' + document.URL + '<br />';
-// Find the date the document was last modified and add it to the msg variable
-msg += '<b>last modified: </b>' + document.lastModified + '</p>';
+var msg = '<p><b>page title: </b>' + document.title + '<br/>';
+msg += '<b><p>page address: </b>' + document.URL + '<br />';
+msg += '<b><p>last modified: </b>' + document.lastModified + '<br />';
 
-// Create a variable called el to hold the element whose id attribute has a value of footer
-var el = document.getElementById('footer');
-// Write the message into that element
-el.innerHTML = msg;
+var footElement = document.getElementById('footer');
+footElement.innerHTML = msg;
