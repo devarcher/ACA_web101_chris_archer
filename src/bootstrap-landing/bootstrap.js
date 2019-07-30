290e75053f42614f1ddacbf128c35562b5b83f53
Chris Archer <archershot@hotmail.com>
2019-07-30 12:03:31 -0500

    started to cleanup font sizing

--- src/bootstrap-landing/bootstrap.js
+++ src/bootstrap-landing/bootstrap.js
@@ -1,7 +1,7 @@
 
 
 function setRotateWord() {
-  var wordList = new Array ("dog.", "owner.", "neighbor.", "friend.", "family.", "vet.", "granparent.", "party.");
+  var wordList = new Array ("dog.", "owner.", "partner.", "community.", "park.", "baby.", "neighbor.", "friend.", "family.", "vet.", "grandparent.", "party.");
   var random = Math.floor(Math.random()*wordList.length);
   $("#rotate").text(wordList[random]);
   console.log(random)
