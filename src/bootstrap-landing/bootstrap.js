84782a8819b7df8b7578953d3689d78480cd627e
Chris Archer <archershot@hotmail.com>
2019-07-30 18:02:10 -0500

    added javascript to cycle through array of words in order

--- src/bootstrap-landing/bootstrap.js
+++ src/bootstrap-landing/bootstrap.js
@@ -1,5 +1,4 @@
-
-
+/* Functional Random Function 
 function setRotateWord() {
   var wordList = new Array ("dog.", "owner.", "partner.", "community.", "park.", "baby.", "neighbor.", "friend.", "family.", "vet.", "grandparent.", "party.");
   var random = Math.floor(Math.random()*wordList.length);
@@ -8,3 +7,25 @@ function setRotateWord() {
 }
 
 setInterval(setRotateWord, 500);
+*/
+
+// Non-Random Rotate Function
+var arr = new Array ("dog.", "owner.", "partner.", "community.", "park.", "baby.", "neighbor.", "friend.", "family.", "vet.", "grandparent.", "party.", "life.");
+
+var i = 0;
+var interval;
+
+function nextItem() {
+  if (i + 1 < arr.length) {
+    i +=1;
+  } else {
+    i = 12;
+    clearInterval(interval);
+  }
+  document.getElementById('rotate').textContent = arr[i];
+}
+
+interval = setInterval(nextItem, 300);
+  document.getElementById('rotate').textContent = arr[i];
+
+ 
